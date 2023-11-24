fe829a1f16dfc20096a63e1de740a9b3a665998b
retroazure <tomascosta005@outlook.com>
2023-11-24 12:09:51 +0000

    Added Component Header, structure setup

--- landing_page/src/main.rs
+++ landing_page/src/main.rs
@@ -1,21 +1,22 @@
 use leptos::*;
 
 fn main() {
-    leptos::mount_to_body(|| view! { <App/> })
+    leptos::mount_to_body(|| view! { <Header/> })
 }
 
+//This is the Main Header of the page
 #[component]
-fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-
+fn Header() -> impl IntoView {
     view! {
-        <button
-        on:click=move |_| {
-            set_count.update(|n| *n += 1);
-        }
-    >
-            "Click me: "
-            {move || count.get()}
-        </button>
+        <div class=("main_header")>
+        <h1>Your Gateway To Decentralized Finance</h1>
+
+        <h4>AirDao Is A Community-Governed Layer One Blockchain And Ecosystem Of Web3 Dapps. Powered By The AMB Token</h4>
+        </div>
+
+        <div class="button_div">
+        <button class="button_style">Trade AMB on firepot</button>
+        <button class="button_style">Use AMB Bridge</button>
+    </div>
     }
 }
