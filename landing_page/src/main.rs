9b0bda14b03be9edfa89d6c078bcac0d1d0d3fd8
retroazure <tomascosta005@outlook.com>
2023-11-27 13:12:34 +0000

    Added New Section Component

--- landing_page/src/main.rs
+++ landing_page/src/main.rs
@@ -1,18 +1,21 @@
 use leptos::*;
 
 fn main() {
-    leptos::mount_to_body(|| view! { 
-        <Navbar />
-        <Header/>
-        <Section />
-     })
+    leptos::mount_to_body(|| {
+        view! {
+           <Navbar />
+           <Header/>
+           <Section />
+           <SecondSection />
+        }
+    })
 }
 
 //This is the Main Header of the page
 #[component]
 fn Header() -> impl IntoView {
     view! {
-        <div class=("main_header")>
+    <div class=("main_header")>
             <h1>Your Gateway To Decentralized Finance</h1>
 
             <h4>AirDao Is A Community-Governed Layer One Blockchain And Ecosystem Of Web3 Dapps. Powered By The AMB Token</h4>
@@ -28,12 +31,14 @@ fn Header() -> impl IntoView {
 #[component]
 fn Section() -> impl IntoView {
     view! {
-        <section class=("main_section")>
+    <section class=("main_section")>
         <div class=("div_section")>
             <h1>Buy AMB On Binance</h1>
             <h4>Trade AMB On Binance. The Worlds Largest Cryptocurrency Exchange</h4>
             <h3>
+            <a href="#">
             Use AMB Bridge
+            </a>
             </h3>
         </div>
         <div class=("div_section")>
@@ -44,17 +49,19 @@ fn Section() -> impl IntoView {
             Securely Bridge AMB Between AirDAO And Compatible Blockchains
             </h4>
             <h3>
+            <a href="#">
             Use AMB Bridge
+            </a>
             </h3>
         </div>
-        </section>
+    </section>
     }
 }
 
 #[component]
 fn Navbar() -> impl IntoView {
     view! {
-        <div class=("nav_div")>
+    <div class=("nav_div")>
         <div>
             <h1>
             AirDAO
@@ -70,6 +77,45 @@ fn Navbar() -> impl IntoView {
 
         <button class=("button_navbar")>Connect Wallet</button>
 
+    </div>
+    }
+}
+
+
+#[component]
+fn SecondSection() -> impl IntoView {
+    view! {
+    <section class=("second_section")>
+        <h1 class=("big_title")>Firepot Swap</h1>
+        <div class=("container_section")>
+        <h3>Buy And Trade AMB And Other Crypto With FirepotSwap.
+        AirDAOs Native Decentralized Exchange.
+        </h3>
+        <div class=("container_buttons")>
+            <button class=("button_style_2")>Use Firepotswap</button>
+            <button class=("button_style_2")>Learn More</button>
         </div>
+        </div>
+        <div class="box_section">
+        <div>
+        <h1 class=("section_headers")>MEXC</h1>
+        <p>MEXC Provides A Platform For Users To Trade A Vast Array Of Cryptocurrencies</p>
+        </div>
+        <div>
+        <h1 class=("section_headers")>Binance</h1>
+        <p>Binance Has Its Own Native Utility Token Called WBTC. 
+        Holders Of WBTC May Enjoy Certain</p>
+        </div>
+        <div>
+        <h1 class=("section_headers")>KuCoin</h1>
+        <p>KuCoin Provides A Platform For Trading A Diverse Selection Of Cryptocurrencies</p>
+        </div>
+        <div>
+        <h1 class=("section_headers")>WhiteBIT</h1>
+        <p>WhiteBit Has Its Own Native Utility Token Called WBTC. 
+        Holders Of WBTC May Enjoy Certain</p>
+        </div>
+        </div>
+    </section>
     }
 }
